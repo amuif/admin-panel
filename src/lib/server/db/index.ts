9cd3a0a2f9816a06c88d50abecb046214b19e7d6
amuif <amudihayat@gmail.com>
2026-04-22 14:56:01 +0300

    fix:rsconfig issue

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-const queryClient = postgres(process.env.DATABASE_URL!, { prepare: false });
+const queryClient = postgres(process.env.DATABASE_URL!, { prepare: true });
 
 export const db = drizzle(queryClient);
 
