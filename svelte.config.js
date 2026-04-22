b3d3aea37f4fff35ab609131a975c951f6223b06
amuif <amudihayat@gmail.com>
2026-04-22 22:21:39 +0300

    fix:svlete.config adopter changed to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: vercel()
 	}
 };
 
